00e7f67affd411adf305ba74eb43f60ac5fa311a
MaqOwais <quadirowais200@gmail.com>
2021-08-12 19:44:09 +0530

    graph-3 d-11

---
+++ A strivers/day-10 grphs/no of island.java
@@ -0,0 +1,75 @@
+/*
+200. Number of Islands
+Medium
+
+9624
+
+263
+
+Add to List
+
+Share
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+*/
+
+
+class Solution {
+
+    public int numIslands(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int cnt = 0;
+        
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < col; c++)
+                if (grid[r][c] == '1') cnt += dfs(grid, r, c);   
+
+        return cnt;
+    }
+    
+    private int dfs(char[][] grid,int r,int c){
+
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != '1') return 0;
+        grid[r][c] = '2'; //  visited is marked as 2 (or) grid[r][c] = '0'; could be used instead
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+        
+        return 1;
+    }
+    
+    
+}
